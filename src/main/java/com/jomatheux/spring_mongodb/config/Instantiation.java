695fc2567cffccb2e38390d707329f3c8b440e27
jomatheux <jomatheux@gmail.com>
2024-08-10 14:20:30 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/jomatheux/spring_mongodb/config/Instantiation.java
+++ src/main/java/com/jomatheux/spring_mongodb/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.jomatheux.spring_mongodb.config;
 
 import com.jomatheux.spring_mongodb.domain.Post;
+import com.jomatheux.spring_mongodb.dto.AuthorDTO;
 import com.jomatheux.spring_mongodb.repository.PostRepository;
 import com.jomatheux.spring_mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,10 +32,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!",maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!",maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
 
