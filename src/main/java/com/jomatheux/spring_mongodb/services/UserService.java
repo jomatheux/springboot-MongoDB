3eab20f74827749ee4138ee8ecded8d1c5807dc9
jomatheux <jomatheux@gmail.com>
2024-08-09 23:58:45 -0300

    Obtendo um usuário por id

--- src/main/java/com/jomatheux/spring_mongodb/services/UserService.java
+++ src/main/java/com/jomatheux/spring_mongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.jomatheux.spring_mongodb.services;
 
 import com.jomatheux.spring_mongodb.domain.User;
 import com.jomatheux.spring_mongodb.repository.UserRepository;
+import com.jomatheux.spring_mongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findOne(String id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
